a9500e20d11e223401d877ce3b206ae72956e4e2
Mandume97 <jeffersonbelo3494@gmail.com>
2021-12-11 08:32:25 -0300

    criando_obj commit

---
+++ Objetos/1_criando_objeto/index.js
@@ -0,0 +1,10 @@
+let carro = {
+    porta: 4,
+    cor: "preto",
+    ano: 2011,
+}
+
+console.log(carro) // Chama o objeto completo
+
+console.log(carro.porta) // chama só a propriedade PORTA q está dentro do obj carro 
+
