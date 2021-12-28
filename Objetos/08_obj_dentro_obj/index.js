d72e4dcbede20efa6c6a7da02092d00b4b85a0c0
Mandume97 <jeffersonbelo3494@gmail.com>
2021-12-28 00:31:15 -0300

    08 commit

---
+++ Objetos/08_obj_dentro_obj/index.js
@@ -0,0 +1,13 @@
+let people = {
+    name: "Jefferson",
+    characteristics: {
+        eyes: "green",
+        hair: "Brown",
+        toys: false,
+        glasses: true
+    }
+}
+console.log(people.characteristics.eyes);
+
+
+
